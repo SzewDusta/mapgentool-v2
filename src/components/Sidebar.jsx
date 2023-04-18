684a55d7c4e3f3f698ee7967bcc7da6d44de1a8a
SaskaKempaZJblka <filip.sek2@gmail.com>
2023-04-19 00:21:32 +0200

    Merge branch 'skrobson' of https://github.com/SzewDusta/mapgentool-v2 into skrobson

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import useImage from 'use-image';
+import ziemia from './ziemia.png';
+import woda from './woda.png';
+import podloga from './podloga.png';
+
+
+export default function Sidebar() {
+    
+  return (
+    <div>
+      {/* <button onClick={App.exportJson} >EKSPORT</button>
+      Wybierz blok:
+      <br />
+      <img
+        alt="podloga"
+        src={podloga}
+        draggable="true"
+        onDragStart={(e) => {
+          App.setAlt(e.target.alt);
+          App.dragUrl.current = e.target.src;
+        }}
+        
+      />
+      <img
+        alt="ziemia"
+        src={ziemia}
+        draggable="true"
+        onDragStart={(e) => {
+          App.setAlt(e.target.alt);
+          App.dragUrl.current = e.target.src;
+        }}
+      ></img>
+      <img
+        alt="woda"
+        name="chuj"
+        src={woda}
+        draggable="true"
+        onDragStart={(e) => {
+          App.setAlt(e.target.alt);
+          App.dragUrl.current = e.target.src;
+        }}
+      /> */}
+    </div>
+  )
+}
